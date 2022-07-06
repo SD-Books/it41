0c05ea66b6dfd19f9ac7c458d68dae40c1eed3fe
IT 4 Supports <101559185+SD-Books@users.noreply.github.com>
2022-07-06 20:25:09 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function myFunction1() {
+    var dots1 = document.getElementById("dots-1");
+    var moreText1 = document.getElementById("more-1");
+    var btnText1 = document.getElementById("myBtn-1");
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText1.innerHTML = "Read more >";
+      moreText1.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText1.innerHTML = "Read less <";
+      moreText1.style.display = "inline";
+    }
+  }
